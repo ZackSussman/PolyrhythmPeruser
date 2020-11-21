b2563c681506cd83208439082309b5a065dbf365
ZackSussman <65458956+ZackSussman@users.noreply.github.com>
2020-11-21 16:19:42 -0500

    Update Synth.py
    smoother sound

--- Synth.py
+++ Synth.py
@@ -1,12 +1,17 @@
 import numpy as np
 
+#data is a numpy list, want to normalize it to a certain max value
+def normalize(data, desiredMax):
+    currentMax = np.max(data)
+    return data * (desiredMax/currentMax)
+
 
 #handle the actual generation of sound data
 class Synthesizer():
     #waveTable is a function which will be used to get the audio values
     #waveTable is a tuple, first coordinate is the period of the wavetable, second is the actual function that is periodic
     #noteOn and noteOff time are both in miliseconds
-    def __init__(self, sampleRate, waveTable, bufferSize, dtype, maxAmplitude):
+    def __init__(self, frequency, sampleRate, waveTable, bufferSize, dtype, maxAmplitude):
         self.sampleRate = sampleRate
         self.dtype = dtype
         self.waveTablePeriod, self.waveTable = waveTable
@@ -17,6 +22,7 @@ class Synthesizer():
         self.fadingOut = False
         self.noteOn = False
         self.amplitudeControlCoef = 1
+        self.frequency = frequency
 
     def turnNoteOn(self):
         self.fadingIn = True
@@ -25,6 +31,8 @@ class Synthesizer():
             self.amplitudeControlCoef = 1 - self.amplitudeControlCoef
             self.fadingOut = False
 
+    def setFrequency(self, freq):
+        self.frequency = freq
     
     def turnNoteOff(self):
         self.fadingOut = True
@@ -33,12 +41,12 @@ class Synthesizer():
             self.fadingIn = False
 
     #get a buffer of audio data at the given frequency
-    def getAudioData(self, frequency):
+    def getAudioData(self):
         if not self.noteOn:
             return np.fromiter([0] * self.bufferSize, dtype = self.dtype)
         
         secondsPerBuffer = self.bufferSize/self.sampleRate
-        cyclesPerBuffer = frequency * secondsPerBuffer
+        cyclesPerBuffer = self.frequency * secondsPerBuffer
         inputCoef = cyclesPerBuffer*self.waveTablePeriod/(self.bufferSize-1)
         audioData = np.fromiter([self.maxAmplitude * self.waveTable(inputCoef*(self.bufferSize*self.bufferNum + x)) for x in range(self.bufferSize)], dtype = self.dtype)
         self.bufferNum += 1
@@ -49,16 +57,15 @@ class Synthesizer():
         if self.fadingIn:
             for i in range(len(audioData)):
                 audioData[i] = self.dtype(audioData[i] * (1 - self.amplitudeControlCoef))
-                self.amplitudeControlCoef *= .9999
-
-            if self.amplitudeControlCoef < 0.001:
+                self.amplitudeControlCoef *= .999
+            if self.amplitudeControlCoef < 0.0001:
                 self.fadingIn = False
                 self.amplitudeControlCoef = 1 - self.amplitudeControlCoef
         else:
             for i in range(len(audioData)):
                 audioData[i] = self.dtype(audioData[i] * self.amplitudeControlCoef)
-                self.amplitudeControlCoef *= .9999
-            if self.amplitudeControlCoef < 0.001:
+                self.amplitudeControlCoef *= .999
+            if self.amplitudeControlCoef < 0.0001:
                 self.fadingOut = False
                 self.noteOn = False
     
